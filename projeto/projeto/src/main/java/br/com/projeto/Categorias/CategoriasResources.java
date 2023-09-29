3e35acd7fa915dea1e2b4b6ff7d3f35767d414a7
JulianaC2075 <julucacalcanti@gmail.com>
2023-09-29 00:36:30 -0300

    Correções

--- projeto/projeto/src/main/java/br/com/projeto/Categorias/CategoriasResources.java
+++ projeto/projeto/src/main/java/br/com/projeto/Categorias/CategoriasResources.java
@@ -1,21 +1,18 @@
-package br.com.projeto;
+package br.com.projeto.Categorias;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//Controlador Rest que responde por Categorias (endpoint(URL, endereço))
-@RequestMapping (value = "/Categorias")
-public class CategoriaResources {
-
-    //Requisição básica
-    // Rest é uma API (ponte de comunicação)
+@RequestMapping(value = "/Categorias")
+public class CategoriasResources {
+    
+// testando a Rest
     @RequestMapping(method = RequestMethod.GET)
     public String listar(){
 
-return "REST está funcionando!";
-
+        return "REST está funcionando!";
     }
     
 }
